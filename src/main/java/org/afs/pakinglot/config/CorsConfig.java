155f63b20ed01f063642bb59336b7281014b925c
Jacky Lee <leeja12@oocl.com>
2024-12-04 15:13:16 +0800

    feat: CORS setting

---
+++ src/main/java/org/afs/pakinglot/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package org.afs.pakinglot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedHeaders()
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization")
+                .exposedHeaders("Authorization")
+                .allowedOrigins("http://localhost:5173")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
